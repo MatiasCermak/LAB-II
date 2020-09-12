193a323ffc05e6704c9a0f7979a6bc14ed766bdc
unknown <matias.cermak99@gmail.com>
2020-09-12 20:01:42 -0300

    login creado

---
+++ login-register-page.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Bootstrap Javascript -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <div class="container-fluid">
+        <header class="row">
+            <div class="col-md-2 col-sm-12 col-12" id="logo"><img src="images/AdventureHubLogo.png" alt="logo.png" id="imglogo"></div>
+            <div class="col-md-3 col-sm-0 col-0" id="buscador">
+                <form>
+                    <input type="text" placeholder="Buscar" style="width: 100%; height: 30px">
+                </form>
+            </div>
+            <div class="col-md-4 col-sm-0 col-0"> Spacer</div>
+            <div class="col-md-3 col-sm-0 col-0">Eres organizador? <a href="#">registrate o inicia sesión</a></div>
+            <div class="col-md-0 col-sm-12 col-12">Menu acordeon</div>
+        </header>
+        <div class="row ">
+            <div class="col-12"> Spacer </div>
+            <div class="col-md-3 col-sm-0 col-0"> Spacer</div>
+            <div class="col-md-6 col-sm-12 col-12">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1" class="">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+            <div class="col-md-3 col-sm-0 col-0"> Spacer</div>
+        </div>
+        <div class="row">
+            <footer class="col-md-12 col-sm-12 col-0"> &copy Propiedad de Matías Cermak y Jeremías Fernandez</footer>
+        </div>
+
+
+
+
+
+
+    </div>
+</body>
