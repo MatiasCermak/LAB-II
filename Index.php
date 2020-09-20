05920f97c942e6f7f369c20b06ce7819ea9d832c
jeresabelox <jeresabelox@gmail.com>
2020-09-20 18:26:14 -0300

    pagina actualizada pero en realidad no

--- Index.php
+++ Index.php
@@ -51,14 +51,13 @@
                         <div class="d-none d-lg-block">
                             <div class="slide-box">
                                 <div class="card" style="width: 18rem;">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/imagen3.png " class="card-img-top " alt="...">
                                     <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make
-                                            up the bulk of the card's content.</p>
+                                        <p class="card-text">Titulo.</p>
                                     </div>
                                 </div>
                                 <div class="card" style="width: 18rem;">
-                                    <img src="..." class="card-img-top" alt="...">
+                                    <img src="images/imagen2.png " class="card-img-top " alt="...">
                                     <div class="card-body">
                                         <p class="card-text">Some quick example text to build on the card title and make
                                             up the bulk of the card's content.</p>
